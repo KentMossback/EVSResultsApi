d4c2e7be3e761efbe83b538f4c51d1c87aa7742a
Kent Mossbäck <kent.mossback@scb.se>
2020-02-19 22:26:31 +0100

    Added database and database migrations

---
+++ EVSResultsApi/Migrations/20200219202218_InitialCreate.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EVSResultsApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "evs");
+
+            migrationBuilder.CreateTable(
+                name: "Group",
+                schema: "evs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Group", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pitch",
+                schema: "evs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pitch", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Team",
+                schema: "evs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Team", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Group",
+                schema: "evs");
+
+            migrationBuilder.DropTable(
+                name: "Pitch",
+                schema: "evs");
+
+            migrationBuilder.DropTable(
+                name: "Team",
+                schema: "evs");
+        }
+    }
+}
---
+++ EVSResultsApi/Repository/TeamRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+
+namespace EVSResultsApi.Repository
+{
+    public class TeamRepository
+    {
+        
+        
+
+
+    }
+}
