a40b89ebca8c0c03442806866b90568dd9a9cffe
Kent Mossbäck <kent.mossback@scb.se>
2020-02-24 09:55:03 +0100

    Adde file cintriller

---
+++ EVSResultsApi/Controllers/FileController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+
+namespace EVSResultsApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("EVS file upload get method");
+        }
+
+        //[HttpPost]
+        //public ActionResult UploadFile()
+        //{
+
+
+        //    return Ok();
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> OnPostUploadAsync()
+        //{
+        //    // Don't rely on or trust the FileName property without validation.
+
+        //    //return Ok();
+        //    return Ok("EVS file upload POST method " );
+        //}
+
+
+        [HttpPost]
+        [Consumes("application/x-www-form-urlencoded")]
+        public async Task<IActionResult> OnPostUploadAsync([FromForm] string xmlFile)
+        {
+            //    // Process uploaded files
+            //    // Don't rely on or trust the FileName property without validation.
+            
+            return Ok();
+            //    return Ok("EVS file upload get method " + new { count = files.Count, size});
+        }
+    }
+
+    //[HttpPost]
+    //    [Consumes("application/x-www-form-urlencoded")]
+    //    public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
+    //    {
+    //        long size = files.Sum(f => f.Length);
+
+    //        foreach (var formFile in files)
+    //        {
+    //            if (formFile.Length > 0)
+    //            {
+    //                var filePath = Path.GetTempFileName();
+
+    //                using (var stream = System.IO.File.Create(filePath))
+    //                {
+    //                    await formFile.CopyToAsync(stream);
+    //                }
+    //            }
+    //        }
+
+    //        //    // Process uploaded files
+    //        //    // Don't rely on or trust the FileName property without validation.
+
+    //        return Ok();
+    //        //    return Ok("EVS file upload get method " + new { count = files.Count, size});
+    //        }
+    //    }
+}
