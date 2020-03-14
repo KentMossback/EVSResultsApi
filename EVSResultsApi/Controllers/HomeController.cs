cf6bfa63de981e3a594854e4afeb37696f97f477
Kent Mossbäck <kent.mossback@scb.se>
2020-03-14 22:45:03 +0100

    More code

--- EVSResultsApi/Controllers/HomeController.cs
+++ EVSResultsApi/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using EVSResultsApi.Models;
+using EVSResultsApi.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EVSResultsApi.Controllers
@@ -10,9 +9,50 @@ namespace EVSResultsApi.Controllers
 
     public class HomeController : Controller
     {
+        private static readonly IList<CommentModel> _comments;
+        //private readonly IRepo _repo;
+
+        static HomeController( )
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Kent Mossbäck",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            _repo.GetTeams()
+
+
+            return Json(_comments);
+        }
+
+
     }
 }
--- EVSResultsApi/wwwroot/js/tutorial.jsx
+++ EVSResultsApi/wwwroot/js/tutorial.jsx
@@ -1,10 +1,17 @@
-﻿class CommentBox extends React.Component {
+﻿const data = [
+    { id: 1, author: 'Kent Mossbäck', text: 'Hello ReactJS.NET World!' },
+    { id: 2, author: 'Pete Hunt', text: 'This is one comment' },
+    { id: 3, author: 'Jordan Walke', text: 'This is *another* comment' },
+];
+
+
+class CommentBox extends React.Component {
     render() {
         return (
             //<div className="commentBox">Hello, world! I am a CommentBox.</div>
             <div className="commentBox">
-                <h1>Comments</h1>
-                <CommentList />
+                <h1>Resultat</h1>
+                <CommentList data={this.props.data} />
                 <CommentForm />
             </div>
         );
@@ -13,18 +20,12 @@
 
 class CommentList extends React.Component {
     render() {
-        return (
-            //<div className="commentList">Hello, world! I am a CommentList.</div>
-            <div className="commentList">
-                <Comment author="Daniel Lo Nigro">
-                    Hello ReactJS.NET World!
-                </Comment>
-                <Comment author="Pete Hunt">This is one comment</Comment>
-                <Comment author="Jordan Walke">
-                    This is *another* comment
-                </Comment>
-            </div>
-        );
+        const commentNodes = this.props.data.map(comment => (
+            <Comment author={comment.author} key={comment.id}>
+                {comment.text}
+            </Comment>
+        ));
+        return <div className="commentList">{commentNodes}</div>;
     }
 }
 
@@ -47,15 +48,19 @@ function createRemarkable() {
 }
 
 class Comment extends React.Component {
+    rawMarkup() {
+        const md = new Remarkable();
+        const rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    }
     render() {
-        //const md = createRemarkable();
         return (
             <div className="comment">
                 <h2 className="commentAuthor">{this.props.author}</h2>
-                {this.props.children}
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
             </div>
         );
     }
 }
 
-ReactDOM.render(<CommentBox />, document.getElementById('content'));
+ReactDOM.render(<CommentBox data={data} />, document.getElementById('content'));
